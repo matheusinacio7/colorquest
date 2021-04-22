2312692444e7bed1ccdd975b4bfd0d614e236db5
heyset <c.inaciomatheus@gmail.com>
2021-04-22 03:00:33 -0300

    Adds assets and resizing function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import { Fragment } from 'react';
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { Fragment, useEffect } from 'react';
+import Head from 'next/head';
+import * as _ from 'lodash';
+import styles from '../styles/Home.module.css';
 
 import Header from '../components/Header';
 import Game from '../components/Game';
@@ -8,6 +9,20 @@ import Article from '../components/Article';
 import Footer from '../components/Footer';
 
 export default function Home() {
+  const fixMainHeight = () => {
+    console.log('resizing');
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+
+  useEffect(() => {
+    fixMainHeight();
+
+    window.addEventListener('resize', _.throttle(fixMainHeight, 200));
+
+    return () => window.removeEventListener('resize', _.throttle(fixMainHeight, 200));
+  }, [fixMainHeight])
+
   return (
     <Fragment>
       <Head>
