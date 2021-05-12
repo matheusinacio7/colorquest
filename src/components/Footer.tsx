0a556e51c8e767d635a66e42188d5b3927aa10c9
heyset <c.inaciomatheus@gmail.com>
2021-05-12 15:11:14 -0300

    Fixes any typing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,16 @@
 import { Fragment } from 'react';
 
+import FoldIcon from '../../assets/svg/double-down-arrow.svg';
 import styles from '../styles/modules/Footer.module.css';
 
 export default function Footer( props: { style: 'main' | 'alt' } ) {
   return (
     <footer className={`${styles.footer} ${props.style === 'alt' ? styles.last : null}`}>
       {props.style === 'main' ? 
-        <Fragment><span>enjoy the game?</span> <a href="https://buymeacoffee.com/heyset" target="_blank"><img src="./svg/bmc-logo.svg" alt="buy me a coffee logo"/> buy me a coffee</a>:)</Fragment>:
+        <Fragment>
+          <span>enjoy the game?</span> <a href="https://buymeacoffee.com/heyset" target="_blank"><img src="./svg/bmc-logo.svg" alt="buy me a coffee logo"/> buy me a coffee</a>:)
+          <FoldIcon />
+        </Fragment>:
         <p>Created with &#x2665; and &#x2615; by <a href="https://github.com/heyset" target="_blank">Matheus "Set" Inacio</a>, 2021.</p>
       }
     </footer>
